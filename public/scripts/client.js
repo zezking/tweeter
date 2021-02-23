c18090cb5ced661411878c1e2b1383d1d257e3ce
zezking <zhaoenze001@gmail.com>
2021-02-22 18:20:05 -0800

    add textarea auto height js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,17 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$(document).ready(function () {
+  $("textarea")
+    .each(function () {
+      console.log("something");
+      this.setAttribute(
+        "style",
+        "height:" + this.scrollHeight + "px;overflow-y:hidden;"
+      );
+    })
+    .on("input", function () {
+      this.style.height = "auto";
+      this.style.height = this.scrollHeight + "px";
+    });
+});
