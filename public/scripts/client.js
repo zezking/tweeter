d6f7727297e1d6c5877c496eafd128cbb0a1b23f
zezking <zhaoenze001@gmail.com>
2021-02-23 12:17:34 -0800

    add const user obejct variable

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,19 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+const newTweetFeed = {
+  user: {
+    name: "Newton",
+    avatars: "https://i.imgur.com/73hZDYK.png",
+    handle: "@SirIsaac",
+  },
+  content: {
+    text: "If I have seen further it is by standing on the shoulders of giants",
+  },
+  created_at: 1461116232227,
+};
+
 $(document).ready(function () {
   //autoheight for text area
 
@@ -15,6 +28,7 @@ $(document).ready(function () {
       this.style.height = this.scrollHeight + 2 + "px";
     });
 
+  //character counter
   $("textarea").on("input", function () {
     let textLength = $(this).val().length;
     let remainingLength = 140 - textLength;
