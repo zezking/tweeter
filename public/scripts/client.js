8c41f9056232a97d0213ad029f6c447f3d992417
zezking <zhaoenze001@gmail.com>
2021-02-22 19:52:06 -0800

    add words counter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
+  //autoheight for text area
+
   $("textarea")
     .each(function () {
       this.setAttribute(
@@ -15,4 +17,10 @@ $(document).ready(function () {
       this.style.height = "auto";
       this.style.height = this.scrollHeight + "px";
     });
+
+  $("textarea").keyup(function () {
+    let textLength = $(this).val().length;
+    let remainingLength = 140 - textLength;
+    $(".counter").text(remainingLength);
+  });
 });
