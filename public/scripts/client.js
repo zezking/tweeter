8a236fb48a36f958eaa267f65df8ff8d6e23ef79
zezking <zhaoenze001@gmail.com>
2021-02-23 13:21:02 -0800

    add createTweetElement and renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,38 +4,33 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
+const tweetData = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
   },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
   },
-  created_at: 1461116232227,
-};
+];
 
-const $tweet = $(`<article class="tweet-feed">
-<header>
-  <div class="user-profile-name">
-    <img src="./images/tweeter-profile3.png" alt="" />
-    <span class="username">Yolo</span>
-  </div>
-  <a class="user-ID">${tweetData.user.name}/a>
-</header>
-<p>${tweetData.content.text}</p>
-<footer>
-  <time>4 days ago</time>
-  <div class="save-retweet-like">
-    <i class="fas fa-bookmark"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-  </div>
-</footer>
-</article>"`);
-
-$(document).ready(function () {
+const $tweet = $(document).ready(function () {
   //autoheight for text area
 
   $("textarea")
@@ -59,4 +54,30 @@ $(document).ready(function () {
     }
     $("#counter").text(remainingLength);
   });
+
+  console.log($tweet); // to see what it looks like
+  $(".new-tweet-feed").append($tweet);
+
+  const renderTweets = (tweets) => {};
+  const createTweetElement = (tweet) => {
+    let $tweet = $(`<article class="tweet-feed">
+    <header>
+      <div class="user-profile-name">
+        <img src="./images/tweeter-profile3.png" alt="" />
+        <span class="username">Yolo</span>
+      </div>
+      <a class="user-ID">${tweetData[0].user.name}</a>
+    </header>
+    <p>${tweetData[0].content.text}</p>
+    <footer>
+      <time>4 days ago</time>
+      <div class="save-retweet-like">
+        <i class="fas fa-bookmark"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+    </article>"`);
+    return $tweet;
+  };
 });
