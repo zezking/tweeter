54d144aa74d046ea6964710d60eb8c139bb1f605
zezking <zhaoenze001@gmail.com>
2021-02-23 15:16:10 -0800

    form post working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,70 +17,77 @@ const tweetData = [
     },
     created_at: 1461116232227,
   },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
 ];
 
-const $tweet = $(document).ready(function () {
-  //autoheight for text area
+$.ajax({
+  url: "https://randomuser.me/api/",
+  dataType: "json",
+  success: function (data) {
+    const randomUserProfile = data.results[0].picture.medium;
+    $(document).ready(function () {
+      //autoheight for text area
+      $("textarea")
+        .each(function () {
+          this.setAttribute("style", "height:30px;overflow-y:hidden;");
+        })
+        .on("input", function () {
+          this.style.height = "30px";
+          this.style.height = this.scrollHeight + 2 + "px";
+        });
 
-  $("textarea")
-    .each(function () {
-      this.setAttribute("style", "height:30px;overflow-y:hidden;");
-    })
-    .on("input", function () {
-      this.style.height = "30px";
-      this.style.height = this.scrollHeight + 2 + "px";
+      //character counter
+      $("textarea").on("input", function () {
+        let textLength = $(this).val().length;
+        let remainingLength = 140 - textLength;
+
+        if (remainingLength < 0) {
+          $("#counter").css("color", "red"); //factor to create a overCounter Class
+        } else {
+          $("#counter").css("color", "black");
+        }
+        $("#counter").text(remainingLength);
+      });
+
+      $("form").on("submit", function (event) {
+        event.preventDefault();
+      });
+
+      renderTweets(tweetData);
     });
 
-  //character counter
-  $("textarea").on("input", function () {
-    let textLength = $(this).val().length;
-    let remainingLength = 140 - textLength;
+    const renderTweets = (tweets) => {
+      for (i in tweets) {
+        $(".new-tweet-feed").append(createTweetElement(tweets[i]));
+      }
+    };
 
-    if (remainingLength < 0) {
-      $("#counter").css("color", "red"); //factor to create a overCounter Class
-    } else {
-      $("#counter").css("color", "black");
-    }
-    $("#counter").text(remainingLength);
-  });
-  //render new tweet feet
-  renderTweets(tweetData);
+    const createTweetElement = (tweet) => {
+      let $tweet = $(`<article class="tweet-feed">
+      <header>
+        <div class="user-profile-name">
+          <img src=${randomUserProfile} alt="" />
+          <span class="username">${tweet.user.name}</span>
+        </div>
+        <a class="user-ID">${tweet.user.handle}</a>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <time>${convertTime(tweet["created_at"])}</time>
+        <div class="save-retweet-like">
+          <i class="fas fa-bookmark"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+      </article>"`);
+      return $tweet;
+    };
+
+    const convertTime = (pastTime) => {
+      const datetime = new Date(0);
+
+      datetime.setUTCSeconds(pastTime);
+      return datetime;
+    };
+  },
 });
-
-const renderTweets = (tweets) => {
-  for (i in tweets) {
-    $(".new-tweet-feed").append(createTweetElement(tweets[i]));
-  }
-};
-const createTweetElement = (tweet) => {
-  let $tweet = $(`<article class="tweet-feed">
-  <header>
-    <div class="user-profile-name">
-      <img src=${tweet.user.avatars} alt="" />
-      <span class="username">${tweet.user.name}</span>
-    </div>
-    <a class="user-ID">${tweet.user.handle}</a>
-  </header>
-  <p>${tweet.content.text}</p>
-  <footer>
-    <time>4 days ago</time>
-    <div class="save-retweet-like">
-      <i class="fas fa-bookmark"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </div>
-  </footer>
-  </article>"`);
-  return $tweet;
-};
