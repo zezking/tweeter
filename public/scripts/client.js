a67ce614da594c820a60ea9b2afaa2b76c919991
zezking <zhaoenze001@gmail.com>
2021-02-23 19:30:58 -0800

    update html placeholder and implement post that fetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,91 +3,80 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const userDataUrl = "https://randomuser.me/api/";
-const tweetData = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-];
+const url = "http://localhost:8080/tweets/";
 
-$.ajax({
-  url: "https://randomuser.me/api/",
-  dataType: "json",
-  success: function (data) {
-    const randomUserProfile = data.results[0].picture.medium;
-    $(document).ready(function () {
-      //autoheight for text area
-      $("textarea")
-        .each(function () {
-          this.setAttribute("style", "height:30px;overflow-y:hidden;");
-        })
-        .on("input", function () {
-          this.style.height = "30px";
-          this.style.height = this.scrollHeight + 2 + "px";
-        });
-
-      //character counter
-      $("textarea").on("input", function () {
-        let textLength = $(this).val().length;
-        let remainingLength = 140 - textLength;
-
-        if (remainingLength < 0) {
-          $("#counter").css("color", "red"); //factor to create a overCounter Class
-        } else {
-          $("#counter").css("color", "black");
-        }
-        $("#counter").text(remainingLength);
-      });
-
-      $("form").on("submit", function (event) {
-        event.preventDefault();
-      });
-
-      renderTweets(tweetData);
+$(document).ready(function () {
+  //autoheight for text area
+  $("textarea")
+    .each(function () {
+      this.setAttribute("style", "height:30px; overflow-y:hidden;");
+    })
+    .on("input", function () {
+      this.style.height = "30px";
+      this.style.height = this.scrollHeight + 2 + "px";
     });
-
-    const renderTweets = (tweets) => {
-      for (i in tweets) {
-        $(".new-tweet-post").append(createTweetElement(tweets[i]));
-      }
-    };
-
-    const createTweetElement = (tweet) => {
-      let $tweet = $(`<article class="tweet-feed">
-      <header>
-        <div class="user-profile-name">
-          <img src=${randomUserProfile} alt="" />
-          <span class="username">${tweet.user.name}</span>
-        </div>
-        <a class="user-ID">${tweet.user.handle}</a>
-      </header>
-      <p>${tweet.content.text}</p>
-      <footer>
-        <time>${convertTime(tweet["created_at"])}</time>
-        <div class="save-retweet-like">
-          <i class="fas fa-bookmark"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-heart"></i>
-        </div>
-      </footer>
-      </article>"`);
-      return $tweet;
-    };
-
-    const convertTime = (pastTime) => {
-      const datetime = new Date(0);
-
-      datetime.setUTCSeconds(pastTime);
-      return datetime;
-    };
-  },
+  //character counter
+  $("textarea").on("input", charCounter);
+  const loadTweets = () => {
+    $.ajax({
+      url,
+      method: "GET",
+    }).done((result) => {
+      renderTweets(result);
+    });
+  };
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    loadTweets();
+  });
 });
+
+//count the character
+const charCounter = function () {
+  let textLength = $(this).val().length;
+  let remainingLength = 140 - textLength;
+
+  if (remainingLength < 0) {
+    $("#counter").css("color", "red"); //factor to create a overCounter Class
+  } else {
+    $("#counter").css("color", "black");
+  }
+  $("#counter").text(remainingLength);
+};
+//convert the unix seconds to normal time
+const convertTime = (pastTime) => {
+  const datetime = new Date(0);
+
+  datetime.setUTCSeconds(pastTime);
+  return datetime;
+};
+const renderTweets = (tweets) => {
+  for (i in tweets) {
+    $(".new-tweet-post")
+      .append(createTweetElement(tweets[i]))
+      .hide()
+      .fadeIn(800);
+  }
+};
+const createTweetElement = (tweet) => {
+  console.log(tweet);
+  let $tweet = $(`<article class="tweet-feed">
+  <header>
+    <div class="user-profile-name">
+      <img src=${tweet.user.avatars} alt="" />
+      <span class="username">${tweet.user.name}</span>
+    </div>
+    <a class="user-ID">${tweet.user.handle}</a>
+  </header>
+  <p>${tweet.content.text}</p>
+  <footer>
+    <time>${convertTime(tweet["created_at"])}</time>
+    <div class="save-retweet-like">
+      <i class="fas fa-bookmark"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>
+  </article>"`);
+  return $tweet;
+};
--- server/lib/util/user-helper.js
+++ server/lib/util/user-helper.js
@@ -1,23 +1,22 @@
 "use strict";
 
-const Chance      = require("chance"),
-      chance      = new Chance();
+const Chance = require("chance"),
+  chance = new Chance();
 
-const md5 = require('md5');
-
+const md5 = require("md5");
 
 module.exports = {
   generateRandomUser: () => {
-    const gender    = chance.gender();
-    const firstName = chance.first({gender: gender});
-    const lastName  = chance.last();
-    const userName  = firstName + " " + lastName;
-    
+    const gender = chance.gender();
+    const firstName = chance.first({ gender: gender });
+    const lastName = chance.last();
+    const userName = firstName + " " + lastName;
+
     let userHandle = "@";
     if (Math.random() > 0.5) {
-      let prefix    = chance.prefix({gender: gender});
+      let prefix = chance.prefix({ gender: gender });
       prefix = prefix.replace(".", "");
-      userHandle += prefix
+      userHandle += prefix;
     }
 
     userHandle += lastName;
@@ -26,22 +25,42 @@ module.exports = {
       const suffix = Math.round(Math.random() * 100);
       userHandle += suffix;
     }
-   
+
     const avatars = {
-    
-      Female: ["https://i.imgur.com/nlhLi3I.png","https://i.imgur.com/z5LNkkB.png","https://i.imgur.com/v0JXau2.png","https://i.imgur.com/lRUnDgU.png", "https://i.imgur.com/3GvwNBf.png"],
-      Male: ["https://i.imgur.com/73hZDYK.png","https://i.imgur.com/5fUVPRP.png","https://i.imgur.com/DVpDmdR.png","https://i.imgur.com/2WZtOD6.png", "https://i.imgur.com/ilT4JDe.png"]
-    
-    }
-    
-    const avatarArray = avatars[gender]
-    const userAvatar = avatarArray[Math.floor(Math.random()*avatarArray.length)]
-  
+      Female: [
+        "https://randomuser.me/api/portraits/med/women/91.jpg",
+        "https://randomuser.me/api/portraits/med/women/14.jpg",
+        "https://randomuser.me/api/portraits/med/women/75.jpg",
+        "https://randomuser.me/api/portraits/med/women/28.jpg",
+        "https://randomuser.me/api/portraits/med/women/29.jpg",
+        "https://randomuser.me/api/portraits/med/women/30.jpg",
+        "https://randomuser.me/api/portraits/med/women/31.jpg",
+        "https://randomuser.me/api/portraits/med/women/32.jpg",
+        "https://randomuser.me/api/portraits/med/women/33.jpg",
+        "https://randomuser.me/api/portraits/med/women/34.jpg",
+      ],
+      Male: [
+        "https://randomuser.me/api/portraits/med/men/30.jpg",
+        "https://randomuser.me/api/portraits/med/men/65.jpg",
+        "https://randomuser.me/api/portraits/med/men/50.jpg",
+        "https://randomuser.me/api/portraits/med/men/74.jpg",
+        "https://randomuser.me/api/portraits/med/men/50.jpg",
+        "https://randomuser.me/api/portraits/med/men/31.jpg",
+        "https://randomuser.me/api/portraits/med/men/62.jpg",
+        "https://randomuser.me/api/portraits/med/men/53.jpg",
+        "https://randomuser.me/api/portraits/med/men/74.jpg",
+        "https://randomuser.me/api/portraits/med/men/55.jpg",
+      ],
+    };
+
+    const avatarArray = avatars[gender];
+    const userAvatar =
+      avatarArray[Math.floor(Math.random() * avatarArray.length)];
 
     return {
       name: userName,
       handle: userHandle,
-      avatars: userAvatar
+      avatars: userAvatar,
     };
-  }
+  },
 };
