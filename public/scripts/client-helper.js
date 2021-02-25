e3feeac2e6dfca4802404d3d879beef8ff3695d6
zezking <zhaoenze001@gmail.com>
2021-02-24 19:43:10 -0800

    update new tweet width

--- public/scripts/client-helper.js
+++ public/scripts/client-helper.js
@@ -113,10 +113,64 @@ const charCounter = function () {
 //convert the unix seconds to normal time // This needs to refactor
 const convertTime = (pastTime) => {
   // 1575909015000
-  const datetime = new Date(pastTime);
-  const humanDateFormat = datetime.toLocaleString();
+  const now = new Date().getTime();
+  const created = pastTime;
+  let howLongAgo = created - now;
+  // Convert to a positive integer
 
-  return humanDateFormat;
+  var getHumanTime = function (timestamp) {
+    // Convert to a positive integer
+    var time = Math.abs(timestamp);
+
+    // Define humanTime and units
+    var humanTime, units;
+
+    // If there are years
+    if (time > 1000 * 60 * 60 * 24 * 365) {
+      humanTime = parseInt(time / (1000 * 60 * 60 * 24 * 365), 10);
+      units = "years";
+    }
+
+    // If there are months
+    else if (time > 1000 * 60 * 60 * 24 * 30) {
+      humanTime = parseInt(time / (1000 * 60 * 60 * 24 * 30), 10);
+      units = "months";
+    }
+
+    // If there are weeks
+    else if (time > 1000 * 60 * 60 * 24 * 7) {
+      humanTime = parseInt(time / (1000 * 60 * 60 * 24 * 7), 10);
+      units = "weeks";
+    }
+
+    // If there are days
+    else if (time > 1000 * 60 * 60 * 24) {
+      humanTime = parseInt(time / (1000 * 60 * 60 * 24), 10);
+      units = "days";
+    }
+
+    // If there are hours
+    else if (time > 1000 * 60 * 60) {
+      humanTime = parseInt(time / (1000 * 60 * 60), 10);
+      units = "hours";
+    }
+
+    // If there are minutes
+    else if (time > 1000 * 60) {
+      humanTime = parseInt(time / (1000 * 60), 10);
+      units = "minutes";
+    }
+
+    // Otherwise, use seconds
+    else {
+      humanTime = parseInt(time / 1000, 10);
+      units = "seconds";
+    }
+
+    return humanTime + " " + units;
+  };
+
+  return getHumanTime(howLongAgo);
 };
 //validate text length if it's too long or too short
 const textValidation = (str) => {
